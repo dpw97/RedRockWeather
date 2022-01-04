cbe429ef04433e58402407aabf2e1c2e8c159c29
David Weaver <dweaver1017@gmail.com>
2022-01-03 16:31:08 -0800

    remove selenium and use cronjobs

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from datetime import datetime
-from selenium.webdriver import Firefox
-from selenium.webdriver.common.by import By
-from os import remove, getenv
+import requests
+from os import remove, getenv, path
 from dotenv import load_dotenv
 import tweepy
 import wget
@@ -11,6 +10,7 @@ CONSUMER_KEY = getenv('CONSUMER_KEY')
 CONSUMER_SECRET = getenv('CONSUMER_SECRET')
 ACCESS_KEY = getenv('ACCESS_KEY')
 ACCESS_SECRET = getenv('ACCESS_SECRET')
+WEATHER_API = getenv('WEATHER_API')
 
 #get formatted date values
 date = datetime.now()
@@ -18,35 +18,35 @@ hour = date.hour
 month = date.strftime('%m')
 day = date.strftime('%d')
 year = date.strftime('%Y')
-def scrape_and_post_tweet():
-    #determine how to format the hours for the URL, either '0800' or '1200'
-    if(hour < 9):
-        fhour = '0' + str(hour-1) + '00'
-    else:
-        fhour = str(hour-1) + '00'
-    partialURL = 'https://cameras-cam.cdn.weatherbug.net/LSVRR/'
+city = 'calico+basin'
 
-    #get formatted image URL then download it
-    imgurl = partialURL + year + '/' + month + '/' + day + \
-            '/' + month + day + year + fhour + '_l.jpg'
-    img = wget.download(imgurl)
-    print('img downloaded')
-    #get weather data using selenium
-    driver = Firefox(executable_path=('./drivers/geckodriver'))
-    weatherURL = 'https://forecast.weather.gov/MapClick.php?lon=-115.433333333&lat=36.1333333333#.YdJ1xJuIYlp'
-    driver.get(weatherURL)
-    temperature = driver.find_element(By.CLASS_NAME, 'myforecast-current-lrg')
-    status = 'The current temperature in Red Rock is ' + temperature.text + '. Photo courtesy of WeatherBug.com'
+#determine how to format the hours for the URL, either '0800' or '1200'
+if(hour < 9):
+    fhour = '0' + str(hour-1) + '00'
+else:
+    fhour = str(hour-1) + '15'
+partialURL = 'https://cameras-cam.cdn.weatherbug.net/LSVRR/'
 
-    # API v1.1 required to upload media to get mediaIDs & post tweet
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-    api = tweepy.API(auth)
+#get formatted image URL then download it
+imgurl = partialURL + year + '/' + month + '/' + day + \
+        '/' + month + day + year + fhour + '_l.jpg'
+img = wget.download(imgurl)
+print('img downloaded')
 
+#get weather data using openweatherAPI
+response = requests.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + WEATHER_API)
+kelvin = (response.json()['main']['temp'])
+fahrenheit = str(round((kelvin - 273.15) * (9/5) + 32)) 
+status = 'The current temperature in Red Rock is ' + fahrenheit + '°F. Photo courtesy of WeatherBug.com'
 
-    #get media id and post tweet
-    media = api.media_upload(img)
-    res = api.update_status(status=status, media_ids=[media.media_id])
-    print('tweet posted')
-    remove(img)
-    driver.close()
+# API v1.1 required to upload media to get mediaIDs & post tweet
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+#get media id and post tweet
+media = api.media_upload(img)
+res = api.update_status(status=status, media_ids=[media.media_id])
+print('tweet posted')
+
+remove(img)
--- run.py
+++
@@ -1,4 +0,0 @@
-from main import scrape_and_post_tweet
-
-def runProgram():
-    scrape_and_post_tweet()
